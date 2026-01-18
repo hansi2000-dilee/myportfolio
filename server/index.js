f260aad88fc0c4700b9111cd3d3349e96f981649
User <user@example.com>
2026-01-18 23:58:09 +0530

    Add start script and fix server static path for Render

--- server/index.js
+++ server/index.js
@@ -639,13 +639,13 @@ app.delete('/api/achievements/image/:id', verifyToken, async (req, res) => {
 });
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
